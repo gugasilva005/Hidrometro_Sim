6ce30c61b143be8ee66702899d2828c3bcb302e5
Gustavo Ferreira Silva <gustavosymoons@gmail.com>
2025-09-27 00:48:44 -0300

    Updates

--- main.cpp
+++ main.cpp
@@ -1,24 +1,19 @@
 #include "Hidrometro.h"
+#include "HidrometroGUI.cpp"
 #include <iostream>
 #include <thread>
 #include <chrono>
 
 
-int main(){
-    // caminho do arquivo config
+int main() {
     std::string cfg = "config.txt";
-
     Hidrometro h(cfg);
-    h.setReadingCallback([](double litros,double vaz,double press) {
-        printf("Leitura -> Volume: %.3f L | Vazao: %.2f mm | Pressao: %.1f kPa", litros, vaz, press);
-        printf("\n");
-    });
 
+    // start da simulação do hidrômetro
     h.start();
 
-    while(true) {
-        std::this_thread::sleep_for(std::chrono::seconds(1));
-    }
+    HidrometroGUI gui(h);
+    gui.run();
 
     h.stop();
     return 0;
